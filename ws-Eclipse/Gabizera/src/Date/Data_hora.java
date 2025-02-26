6552460b1dfc3442eb514f27f972256873de1a4e
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2025-02-26 16:41:06 -0300

    Data hora em java

--- ws-Eclipse/Gabizera/src/Date/Data_hora.java
+++ ws-Eclipse/Gabizera/src/Date/Data_hora.java
@@ -1,9 +1,16 @@
 package Date;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Data_hora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate d01 = LocalDate.parse("20/07/2022",fmt1);
+		
+		System.out.println(d01);
+				
 
 	}
 
