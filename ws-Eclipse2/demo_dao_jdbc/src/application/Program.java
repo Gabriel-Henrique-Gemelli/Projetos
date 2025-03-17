16b3906c2748b103762d620067579662f58af4dc
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2025-03-17 15:43:38 -0300

    Criando Seller

--- ws-Eclipse2/demo_dao_jdbc/src/application/Program.java
+++ ws-Eclipse2/demo_dao_jdbc/src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21,"Bob","Bob@gmai.com",3000.0,new Date(),obj);
+		System.out.println(seller);
 
 	}
 
