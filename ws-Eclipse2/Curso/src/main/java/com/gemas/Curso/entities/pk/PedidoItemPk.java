05dc790ea9d70a7581c3fe13fda1fc43c970d5e5
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2025-03-18 15:33:57 -0300

    OrderItem

---
+++ ws-Eclipse2/Curso/src/main/java/com/gemas/Curso/entities/pk/PedidoItemPk.java
@@ -0,0 +1,55 @@
+package com.gemas.Curso.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.gemas.Curso.entities.Pedido;
+import com.gemas.Curso.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class PedidoItemPk implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Pedido pedido;
+	
+	@ManyToOne
+	@JoinColumn
+	private Product product;
+	
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(pedido, product);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoItemPk other = (PedidoItemPk) obj;
+		return Objects.equals(pedido, other.pedido) && Objects.equals(product, other.product);
+	}
+
+	
+}
